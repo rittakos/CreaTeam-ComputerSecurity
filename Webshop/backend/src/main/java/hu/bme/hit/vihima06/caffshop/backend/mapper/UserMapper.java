fc5eeb0a592950f74b92d6efb2e50909c1ac6add
Bálint Jeszenszky <kisjeszeni1234@gmail.com>
2022-11-11 20:03:21 +0100

    openapi extended with file and admin routes

--- Webshop/backend/src/main/java/hu/bme/hit/vihima06/caffshop/backend/mapper/UserMapper.java
+++ Webshop/backend/src/main/java/hu/bme/hit/vihima06/caffshop/backend/mapper/UserMapper.java
@@ -2,7 +2,7 @@ package hu.bme.hit.vihima06.caffshop.backend.mapper;
 
 import hu.bme.hit.vihima06.caffshop.backend.model.Role;
 import hu.bme.hit.vihima06.caffshop.backend.model.User;
-import hu.bme.hit.vihima06.caffshop.backend.models.LoginResponseUserDetails;
+import hu.bme.hit.vihima06.caffshop.backend.models.UserDetailsResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -13,7 +13,7 @@ import java.util.Set;
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-    LoginResponseUserDetails userToUserDetailsResponse(User user);
+    UserDetailsResponse userToUserDetailsResponse(User user);
 
     default List<String> mapRoles(Set<Role> roles) {
         return roles.stream().map(role -> role.getName().name()).toList();
