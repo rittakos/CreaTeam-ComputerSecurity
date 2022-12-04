cb2a7e046494260a154dc7afcd08dbaba91f0973
Wágner Réka <wgnreka@gmail.com>
2022-12-04 18:22:15 +0100

    feat: fix pr comments

--- Webshop/frontend/src/app/app-routing.module.ts
+++ Webshop/frontend/src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AccessGuard } from './access-guard';
-import { AuthComponent } from './components/auth/auth.component';
-import { HomeComponent } from './components/home/home.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AccessGuard} from './access-guard';
+import {AuthComponent} from './components/auth/auth.component';
 
 // add requiresAdmin: true into the data object instead of requiresLogin if admin role required
 const routes: Routes = [
-  { path: '', component: HomeComponent, data: { requiresLogin: true, redirectTo: '/auth' }, canActivate: [ AccessGuard ] },
+  // { path: '', component: HomeComponent, data: { requiresLogin: true, redirectTo: '/auth' }, canActivate: [ AccessGuard ] },
   { path: 'auth', component: AuthComponent, data: { requiresLogout: true, redirectTo: '/' }, canActivate: [ AccessGuard ], runGuardsAndResolvers: 'always' },
-  { path: 'summary', loadChildren: () => import('../modules/list-view/list-view.module').then(m => m.ListViewModule)},
-  { path: 'products', loadChildren: () => import('../modules/product-view/details.module').then(m => m.DetailsModule)},
-  { path: 'profile', loadChildren: () => import('../modules/profile/profile.module').then(m => m.ProfileModule)},
+  { path: '', loadChildren: () => import('../modules/list-view/list-view.module').then(m => m.ListViewModule), data: { requiresLogin: true, redirectTo: '/auth' }, canActivate:[AccessGuard]},
+  { path: 'products', loadChildren: () => import('../modules/product-view/details.module').then(m => m.DetailsModule), data: { requiresLogin: true, redirectTo: '/auth' }, canActivate:[AccessGuard]},
+  { path: 'profile', loadChildren: () => import('../modules/profile/profile.module').then(m => m.ProfileModule), data: { requiresLogin: true, redirectTo: '/auth' }, canActivate:[AccessGuard]},
 ];
 
 @NgModule({
